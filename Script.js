866f0fdbb7a46d3b2993f083b87e8651138e50e9
Pablo Valdez <pablovaldez0301@gmail.com>
2024-12-18 11:22:08 -0300

    Summary

--- Script.js
+++ Script.js
@@ -3,19 +3,17 @@ const progressGoal = 100; // Meta total
 const progressText = document.getElementById('progress-text');
 const progressFill = document.getElementById('progress-fill');
 
-// Escucha eventos de suscripciones
 window.addEventListener('onEventReceived', function (obj) {
     if (!obj.detail.event) return;
     const listener = obj.detail.listener.split("-")[0];
     const event = obj.detail.event;
 
     if (listener === 'subscriber') {
-        currentProgress += event.amount || 1; // Incrementa según cantidad de suscripciones
+        currentProgress += event.amount || 1; //++ subs
         updateProgress();
     }
 });
 
-// Actualiza la barra de progreso
 function updateProgress() {
     const progressPercentage = Math.min((currentProgress / progressGoal) * 100, 100);
     progressFill.style.width = progressPercentage + '%';
